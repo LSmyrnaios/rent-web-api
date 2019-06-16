c7a32664f33ffd4cece1e71314e89f4422177726
LSmyrnaios <lsmyrnaios@gmail.com>
2019-06-17 00:20:33 +0300

    Add "uploadHotelPhotos"-endpoint. Upload multiple photos with a single request.

--- src/main/java/gr/uoa/di/rent/controllers/HotelController.java
+++ src/main/java/gr/uoa/di/rent/controllers/HotelController.java
@@ -1,27 +1,29 @@
 package gr.uoa.di.rent.controllers;
 
 import gr.uoa.di.rent.exceptions.BadRequestException;
+import gr.uoa.di.rent.exceptions.NotAuthorizedException;
+import gr.uoa.di.rent.exceptions.UserNotExistException;
 import gr.uoa.di.rent.models.*;
 import gr.uoa.di.rent.payload.requests.HotelRequest;
 import gr.uoa.di.rent.payload.requests.filters.PagedHotelsFilter;
-import gr.uoa.di.rent.payload.responses.AmenitiesCount;
-import gr.uoa.di.rent.payload.responses.HotelResponse;
-import gr.uoa.di.rent.payload.responses.PagedResponse;
-import gr.uoa.di.rent.payload.responses.SearchResponse;
+import gr.uoa.di.rent.payload.responses.*;
 import gr.uoa.di.rent.repositories.*;
 import gr.uoa.di.rent.security.CurrentUser;
 import gr.uoa.di.rent.security.Principal;
 import gr.uoa.di.rent.util.AppConstants;
 import gr.uoa.di.rent.util.ModelMapper;
 import gr.uoa.di.rent.util.PaginatedResponseUtil;
+import org.apache.commons.io.FilenameUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import java.lang.reflect.Field;
@@ -42,15 +44,24 @@ public class HotelController {
 
     private final AmenitiesRepository amenitiesRepository;
 
+    private final UserRepository userRepository;
+
+    private final FileController fileController;
+
+    private final FileRepository fileRepository;
+
     private final AtomicInteger counter = new AtomicInteger();
 
-    public HotelController(BusinessRepository businessRepository,
-                           HotelRepository hotelRepository,
-                           AmenitiesRepository amenitiesRepository) {
+    private static String hotelBaseURI = "https://localhost:8443/api/hotels/";
+
+    public HotelController(BusinessRepository businessRepository, HotelRepository hotelRepository, UserRepository userRepository,
+                           AmenitiesRepository amenitiesRepository, FileController fileController, FileRepository fileRepository) {
         this.businessRepository = businessRepository;
         this.hotelRepository = hotelRepository;
         this.amenitiesRepository = amenitiesRepository;
-
+        this.fileController = fileController;
+        this.userRepository = userRepository;
+        this.fileRepository = fileRepository;
     }
 
 
@@ -195,4 +206,76 @@ public class HotelController {
                         hotels.getTotalPages(), hotels.isLast())
         );
     }
+
+
+    @PostMapping("/{hotelId:[\\d]+}/photos")
+    @PreAuthorize("hasRole('PROVIDER') or hasRole('ADMIN')")
+    public List<ResponseEntity<?>> uploadHotelPhotos(@RequestParam("files") MultipartFile[] files, @PathVariable(value = "hotelId") Long hotelId, @Valid @CurrentUser Principal principal) {
+
+        List<ResponseEntity<?>> responses = new ArrayList<>();
+
+        if ( files == null || files.length == 0 ) {
+            String errorMessage = "No files received!";
+            logger.error(errorMessage);
+            responses.add(ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage));
+            return responses;
+        }
+
+        // First check if the hotel exists.
+        Optional<Hotel> hotel_opt = hotelRepository.findById(hotelId);
+        if ( !hotel_opt.isPresent() ) {
+            String errorMessage = "No hotel exists with id = " + hotelId;
+            logger.error(errorMessage);
+            responses.add(ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage));
+            return responses;
+        }
+        Hotel hotel = hotel_opt.get();  // Used also for file-insertion later.
+
+        // Check if the provider which will have a new photo for its hotel, exists or not.
+        Long providerId = hotel.getBusiness().getProvider().getId();
+
+        // If current provider is not Admin and the given "hotelId" doesn't belong to a hotel owned by the current provider requesting, then return error.
+        if ( !principal.getUser().getId().equals(providerId) && !principal.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_ADMIN")) ) {
+            throw new NotAuthorizedException("You are not authorized to update the data of another user!");
+        }
+
+        // Get the provider from the database in order to get passed to the "uploadFile()".
+        User user = userRepository.findById(providerId)
+                .orElseThrow(() -> new UserNotExistException("User with id <" + providerId + "> does not exist!"));
+
+        for ( int i = 0 ; i < files.length ; i++ )
+        {
+            MultipartFile file = files[i];
+
+            if ( file == null ) {
+                logger.warn("Found a null-file in files-list..! Continuing with the next..");
+                continue;
+            }
+
+            // Change the filename to have an incremental value, specific for this hotel.
+            String fileName = file.getOriginalFilename();
+            if ( fileName == null ) {
+                String errorMessage = "Failure when retrieving the filename of the incoming file!";
+                logger.error(errorMessage);
+                responses.add(ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage));
+                continue;
+            }
+
+            String fileExtension = FilenameUtils.getExtension(fileName)
+                    .toLowerCase(); // Linux are case insensitive, so make all file-extensions to lowerCase.
+
+            fileName = (i+1) + "." + fileExtension;
+
+            String fileDownloadURI = hotelBaseURI + hotelId + "/photos/" + fileName;
+
+            // Send file to be stored. We set a new principal in order for the following method to know the user-provider who will have a new photo for its hotel, who provider, might not be the current user (the current user might be the admin who changes the photo of a hotel).
+            responses.add(fileController.uploadFile(Principal.getInstance(user), file, fileName, null, fileDownloadURI, hotel, null));
+        }
+
+        return responses;
+    }
+
+
+    // TODO - Implement endpoint to return multiple photos of a hotel.
+
 }
